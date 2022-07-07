6eb89cd33fbb1a11bc34093c1dd86abc22b9e598
AlexandrPoddubnyi <San4oss91@gmail.com>
2022-07-07 12:54:43 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,18 +3,24 @@ import throttle from "lodash.throttle"
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+// let second = 0;
 
 
 const timeUpdating = function (data) {
-    // console.log(data);
+    console.log(data);
     localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
-}
+};
 
 player.on('timeupdate', throttle(timeUpdating, 1000));
 
 const currentTime = localStorage.getItem('videoplayer-current-time');
-// console.log(currentTime);
-const currentTimeInSeconds = JSON.parse(currentTime).seconds;
-// console.log(currentTimeInSeconds)
+console.log(currentTime);
+
+
+const currentTimeInSeconds = JSON.parse(currentTime)?.seconds || 0;
+console.log(currentTimeInSeconds)
+
+
+// player.getCurrentTime(second);
 
 player.setCurrentTime(currentTimeInSeconds);
